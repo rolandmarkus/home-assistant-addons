55e6aabc123d740d2420785eb861d9b1e1514c8a
rolandmarkus <roland.e.a.markus@gmail.com>
2026-02-08 17:03:37 +0100

    Update run.sh

--- romm/run.sh
+++ romm/run.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/with-contenv bashio
 
+# Secret Key
 export ROMM_AUTH_SECRET_KEY="$(bashio::config 'auth_secret_key')"
 
+# Database config
 export DB_HOST="$(bashio::config 'db_host')"
 export DB_PORT="$(bashio::config 'db_port')"
 export DB_NAME="$(bashio::config 'db_name')"
 export DB_USER="$(bashio::config 'db_user')"
 export DB_PASSWORD="$(bashio::config 'db_password')"
 
+# Bind address
 export ROMM_HOST="0.0.0.0"
 export ROMM_PORT="3000"
 
